f214b949100e2ee324baf1baf274812ac5e0a6c5
nikhilc523 <nikhilchowdary523@icloud.com>
2022-11-25 23:10:37 +0530

    final

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ const express = require("express");
 var app = express();
 var bodyparser = require('body-parser');
 var mysql = require('mysql');
+const fs = require('fs');
 const port = process.env.PORT || 3306;
-var conn=mysql.createConnection({host:"thecommute.mysql.database.azure.com", user:"amritassr", password:"Kaushal@2003", database:"ssramrita", port:3306, ssl:{ca:fs.readFileSync("{ca-cert filename}")}});
+app.use(express.static('public'));
+app.use(express.static('publice'));
+app.use(express.static('publicee'));
+var conn=mysql.createConnection({host:"thecommute.mysql.database.azure.com", user:"amritassr", password:"Kaushal@2003", database:"ssramrita", port:3306});
 app.listen(port);
+conn.connect(function(err){
+  if(err)throw err;
+  console.log("connected");
+})
+app.get('/',function(req,res){
+res.sendFile(__dirname+'/publicee/index.html')
+})
