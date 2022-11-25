32b87e0b1c27b9dcaffd418a45e6911b9988ab61
nikhilc523 <nikhilchowdary523@icloud.com>
2022-11-25 22:21:16 +0530

    kaushal

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ var bodyparser = require('body-parser');
 var mysql = require('mysql');
 const port = process.env.PORT || 5000;
 const db = mysql.createPool({
-  host:'localhost',
-  user:'root',
-  password:'',
+  host:'thecommute.mysql.database.azure.com',
+  user:'amritassr',
+  password:'Kaushal@2003',
   database:'mysql'
 })
 db.getConnection(function(error){
@@ -16,54 +16,8 @@ db.getConnection(function(error){
   throw error;
   }
 })
-app.use(express.static('public'));
-app.use(express.static('publice'));
-app.use(express.static('publicee'));
-app.get('/',function(req,res){
-res.sendFile(__dirname+'/publicee/index.html')
-})
 
-app.get('/find.html',function(req,res){
-res.sendFile(__dirname+'/public/find.html')
-})
 
-app.get('/Add.html',function(req,res){
-res.sendFile(__dirname+'/public/Add.html')
-})
-app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({extended:true}))
-app.use(express.static('public'));
-app.post('/find.html',function(req,res){
-  var desti = req.body.destination;
-  var sour = req.body.source;
-  var date = req.body.birthday;
-  db.getConnection(function(error){
-    if(error){
-    throw error;
-    }
-    var sql = "INSERT INTO ridesharing (Source,Destination,Date) VALUES('"+desti+"','"+sour+"','"+date+"')";
-    db.query(sql,function(error,result){
-      if(error) throw error;
-      console.log("connected");
-      res.send("Ride added Successfully");
 
-    })
 
-  })
-});
-app.get('/Add.html',function(req,res){
-  db.getConnection(function(error){
-    if(error){
-    throw error;
-    }
-    var sql = "SELECT * FROM ridesharing";
-    db.query(sql,function(error,result){
-      if(error) throw error;
-      console.log("result");
-      res.send("Ride added Successfully");
-
-    })
-
-  })
-});
-app.listen(5000);
+app.listen(port);
