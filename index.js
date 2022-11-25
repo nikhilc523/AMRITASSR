d1d6839bedb136de23064bf8a36825bcb0583a0f
nikhilc523 <nikhilchowdary523@icloud.com>
2022-11-25 22:46:40 +0530

    final

--- index.js
+++ index.js
@@ -4,20 +4,6 @@ const express = require("express");
 var app = express();
 var bodyparser = require('body-parser');
 var mysql = require('mysql');
-const port = process.env.PORT || 5000;
-const db = mysql.createPool({
-  host:'thecommute.mysql.database.azure.com',
-  user:'amritassr',
-  password:'Kaushal@2003',
-  database:'mysql'
-})
-db.getConnection(function(error){
-  if(error){
-  throw error;
-  }
-})
-
-
-
-
+const port = process.env.PORT || 3306;
+var conn=mysql.createConnection({host:"thecommute.mysql.database.azure.com", user:"amritassr", password:"Kaushal@2003", database:"ssramrita", port:3306, ssl:{ca:fs.readFileSync("{ca-cert filename}")}});
 app.listen(port);
